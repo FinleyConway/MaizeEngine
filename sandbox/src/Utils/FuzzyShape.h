629190848032c099998b91c298eb942d4cb8a5a0
FinleyConway <finleyconway03@gmail.com>
2025-01-25 20:44:57 +0000

    Encapsulate FuzzyShape

--- sandbox/src/Utils/FuzzyShape.h
+++ sandbox/src/Utils/FuzzyShape.h
@@ -4,16 +4,10 @@
 
 struct FuzzyShape
 {
-    uint8_t shape = 0;
-    uint8_t mask = 0;
-    std::array<Rail::Type, 9> quadrants;
-
-    FuzzyShape() = default;
-
+public:
     FuzzyShape(uint8_t shape, uint8_t mask, std::array<Rail::Type, 9>&& quadrants)
-        : shape(shape), mask(mask), quadrants(std::move(quadrants))
+        : m_Shape(shape), m_Mask(mask), m_Quadrants(std::move(quadrants))
     {
-
     }
 
     Rail::Type GetType(uint8_t x, uint8_t y) const
@@ -21,16 +15,21 @@ struct FuzzyShape
         // out of bounds
         if (x > 2 || y > 2) return Rail::Type::None;
 
-        return quadrants[x + y * 3];
+        return m_Quadrants[x + y * 3];
     }
 
     bool Match(uint8_t bitset) const
     {
-        return (shape & mask) == (bitset & mask);
+        return (m_Shape & m_Mask) == (bitset & m_Mask);
     }
 
     bool operator==(const FuzzyShape& other) const
     {
-        return (shape & mask) == (other.shape & other.mask);
+        return (m_Shape & m_Mask) == (other.m_Shape & other.m_Mask);
     }
+
+private:
+    uint8_t m_Shape = 0;
+    uint8_t m_Mask = 0;
+    std::array<Rail::Type, 9> m_Quadrants;
 };
